bc4c11ae60265c203470174f70c006adf58b53aa
Oscar Llorente <llorente_oscar@hotmail.com>
2018-11-12 21:17:47 -0500

    Instalamos y agregamos Morgan para ver los mensajes por consola

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 4000);
 
 // Middlewares
+app.use(morgan('dev'));
 
 // Routes
 
