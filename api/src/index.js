f3bce272201a79dad566a920e30a91ca71e74b45
Oscar Llorente <llorente_oscar@hotmail.com>
2018-11-13 09:06:49 -0500

    Se configura Express con el puerto que de  el servidor y un por defecto por si este no lo tiene

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,14 @@
-console.log('MEAN APP');
+const express = require('express');
+const app = express();
 
 // Settings
+app.set('port', process.env.PORT || 4000);
 
 // Middlewares
 
 // Routes
 
 // Starting server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.set('port'));
+});
